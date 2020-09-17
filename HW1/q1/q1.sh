7cb23ba922a298c97ecb53a93c00c9107931feca
Aayushd18 <63910239+Aayushd18@users.noreply.github.com>
2020-09-17 13:02:45 +0530

    Add files via upload

---
+++ HW1/q1/q1.sh
@@ -0,0 +1,20 @@
+mkdir q1
+git init
+cd q1
+mkdir Aayush
+cd Aayush
+echo "iitbhilai" >>11940010.txt
+cd ..
+cp -R Aayush Santaz
+cd Santaz
+mv  11940010.txt 11940230.txt
+cd ..
+cp -R Aayush Shubham
+cd Shubham
+mv 1190010.txt 11941140.txt
+cd ..
+git add Aayush
+git add Santaz
+git add Shubham
+git commit -m "All"
+git graph
