1665094342a49c818967aaaae13e92ad92cfd782
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2020-10-10 21:15:54 +0530

    Add files via upload

---
+++ CS200 Git Project/Family_Tree.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+source Init.sh
+source Adding_Of_One_Child.sh
+source Adding_Of_Two_Child.sh
+source Adding_Of_Wife.sh
+source Adding_Of_Husband.sh
+source Make_Tree.sh
+source Exit.sh
+
+initialise
+make_tree
+
+
