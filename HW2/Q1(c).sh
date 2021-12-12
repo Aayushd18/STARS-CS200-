b96bb7f38e5ab7fec3e8e40eaafb6bfaa620b341
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2021-12-12 15:35:43 +0530

    Delete HW2 directory

--- HW2/Q1(c).sh
+++
@@ -1,9 +0,0 @@
-git init Q1
-cd Q1
-touch first.txt
-echo "hello">>first.txt
-git add --patch first.txt
-echo "google">>first.txt
-git add -N first.txt
-git add --patch first.txt
-git graph
--- HW2/Readme.txt
+++
@@ -1,1 +0,0 @@
-This folder will contain the codes and Latex files associated with Second Assignment of Git.
